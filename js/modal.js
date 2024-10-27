f7886b311d1ab165404b04701a73f17f20abf546
Kate <kate.dovzhenk@gmail.com>
2024-10-27 15:12:40 -0400

    fixed some minor problems

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,9 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFee = 50;
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+const message = 'You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.'
